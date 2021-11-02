7746e413e42ca00794c30dc02a84714401f87623
wniy <viniberizzi@gmail.com>
2021-11-02 07:14:24 -0300

    add basic engine functions

--- src/pixel_bot.py
+++ src/pixel_bot.py
@@ -1,6 +1,21 @@
-# class PixelBot:
-#     def __init__(self):
+import psutil
+
+class PixelBot:
+    def __init__(self, yuumi):
+        self.yuumi = yuumi
+
+    def __is_ingame():
+        for proc in psutil.process_iter():
+            try:
+                if 'League of Legends' in proc.name():
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return False
         
-#     def game_init:
+    def game_init(self):
+        self.yuumi.buy_items()
     
-#     def play_game:
+    def play_game(self):
+        while self.__is_ingame():
+            self.yuumi.play()
