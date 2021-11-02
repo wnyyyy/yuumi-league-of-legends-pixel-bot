f83708d1ff556f80aade7059191f1aa212410b6b
wniy <viniberizzi@gmail.com>
2021-11-02 06:42:32 -0300

    first commit

--- src/helpers/mouse_helper.py
+++ src/helpers/mouse_helper.py
@@ -1,4 +1,3 @@
-import os
 import time
 import logging
 import win32api, win32con
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import logging
+
+width = 1281
+height = 721
+
+
+logging.info("Starting game...")
+# yuumi = Yuumi()
+# pixelBot = PixelBot(yuumi)
+# pixelBot.game_init()
+# pixelBot.play_game()
