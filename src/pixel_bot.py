3546dee7c70951a2aa6b643dee77bbe35aa91f92
wniy <viniberizzi@gmail.com>
2021-11-02 07:36:56 -0300

    add comments and bot class

--- src/pixel_bot.py
+++ src/pixel_bot.py
@@ -1,9 +1,11 @@
 import psutil
 
+# classe da engine
 class PixelBot:
     def __init__(self, yuumi):
         self.yuumi = yuumi
 
+    # checa se processo do jogo existe
     def __is_ingame():
         for proc in psutil.process_iter():
             try:
@@ -13,9 +15,11 @@ class PixelBot:
                 pass
         return False
         
+    # primeira ação do bot ao entrar na partida
     def game_init(self):
         self.yuumi.buy_items()
     
+    # loop que faz o bot agir enquanto o jogo estiver aberto
     def play_game(self):
         while self.__is_ingame():
             self.yuumi.play()
---
+++ src/yuumi.py
@@ -0,0 +1,20 @@
+import time
+from helpers.keyboard import keyboard_helper as keyboard
+
+# classe que contém as funcionalidades do bot
+class Yuumi:
+    
+    # itera pela string de prioridade e pressiona ctrl + tecla para cada skill, em ordem de prioridade
+    def __level_up(priority: str):        
+        for char in priority:
+            keyboard.pressHoldRelease_ingame('ctrl', char)
+            time.sleep(.05)
+
+    # função executada para comandar o bot
+    def play(self):
+
+        # upa skills prioridade para upar skills é R > E > W > Q
+        self.__level_up('rewq')
+
+        # timer para manter um tempo mínimo entre as ações
+        time.sleep(.5)
