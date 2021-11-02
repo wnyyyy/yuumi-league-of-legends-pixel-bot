cd6b30c5b7eb303d533b756508789d993eafc963
wniy <viniberizzi@gmail.com>
2021-11-02 07:58:01 -0300

    add pointer coordinates debugging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import logging
+import time
+from helpers.mouse_helper import MouseHelper
 
 width = 1281
 height = 721
+x_pad = 319
+y_pad = 154
 
 
 logging.info("Starting game...")
@@ -9,3 +13,9 @@ logging.info("Starting game...")
 # pixelBot = PixelBot(yuumi)
 # pixelBot.game_init()
 # pixelBot.play_game()
+
+mouse_helper = MouseHelper(x_pad, y_pad)
+# loga coordenadas do cursor para debug
+while True:
+    mouse_helper.get_coords()
+    time.sleep(0.5)
