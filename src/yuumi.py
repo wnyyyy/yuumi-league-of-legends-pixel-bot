1a951b1dd29e16e06de2369fd3103cb661655990
wniy <viniberizzi@gmail.com>
2021-11-03 16:28:59 -0300

    add w status function

--- src/yuumi.py
+++ src/yuumi.py
@@ -8,6 +8,16 @@ class Yuumi:
 
     def __init__(self):
         self.attached = None
+        self.img_p = ImageProcessing(1600, 900)
+
+    def __current_w_status(self):
+        hash = self.img_p.get_box_hash(CH_W_UL[0], CH_W_UL[1], CH_W_LR[0], CH_W_LR[1])
+        if (hash == 9953):
+            print('w status: attached')
+        if (hash == 9378):
+            print('w status: skill up')
+        if (hash == 2345):
+            print('w status: casting')
     
     # itera pela string de prioridade e pressiona ctrl + tecla para cada skill, em ordem de prioridade
     def __level_up(priority: str):        
@@ -18,8 +28,9 @@ class Yuumi:
     # função executada para comandar o bot
     def play(self):
 
-        img_p = ImageProcessing(720, 1280)
-        img_p.get_mean_pixel_value(CH_W_UL[0], CH_W_UL[1], CH_W_LR[0], CH_W_LR[1])
+        # img_p = ImageProcessing(1600, 900)
+        # img_p.get_box_hash(CH_W_UL[0], CH_W_UL[1], CH_W_LR[0], CH_W_LR[1])
+        self.__current_w_status()
 
         # upa skills prioridade para upar skills é R > E > W > Q
         #self.__level_up('rewq')
