7af1cd54e4a06dee1afe718262d12395b7d7daa9
wniy <viniberizzi@gmail.com>
2021-11-03 13:17:24 -0300

    add w coordinates

--- src/coordinates.py
+++ src/coordinates.py
@@ -8,3 +8,11 @@ SHOP_FIRST_BUY = (300, 304)
 SHOP_TAB_RECOMENDADOS = (220, 113)
 
 # minimapa
+TEAM_ALLY_1 = (1125, 498) # top
+TEAM_ALLY_2 = (1166, 498) # jg
+TEAM_ALLY_3 = (1212, 498) # mid
+TEAM_ALLY_4 = (1260, 498) # adc
+
+# champion
+CH_W_UL = (575, 667)
+CH_W_LR = (584, 678)
