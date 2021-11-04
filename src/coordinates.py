21742235bfd630a021a911c45d6fa1c467fa4c43
wniy <viniberizzi@gmail.com>
2021-11-04 00:11:33 -0300

    update coordinates

--- src/coordinates.py
+++ src/coordinates.py
@@ -8,10 +8,10 @@ SHOP_FIRST_BUY = (300, 304)
 SHOP_TAB_RECOMENDADOS = (220, 113)
 
 # minimapa
-TEAM_ALLY_1 = (1390, 600) # top
-TEAM_ALLY_2 = (1449, 600) # jg
-TEAM_ALLY_3 = (1510, 600) # mid
-TEAM_ALLY_4 = (1568, 600) # adc
+TEAM_ALLY_1 = (1314, 496) # top
+TEAM_ALLY_2 = (1390, 496) # jg
+TEAM_ALLY_3 = (1473, 496) # mid
+TEAM_ALLY_4 = (1561, 496) # adc
 
 # champion
 # 9953 - attached, 9378 - skill up, 2345 - casting
