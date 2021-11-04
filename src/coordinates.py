6ece2c23c602c8d21a141724d7d21498ea7588ec
wniy <viniberizzi@gmail.com>
2021-11-04 03:50:05 -0300

    add sides coordinates

--- src/coordinates.py
+++ src/coordinates.py
@@ -12,6 +12,13 @@ TEAM_ALLY_1 = (1314, 496) # top
 TEAM_ALLY_2 = (1390, 496) # jg
 TEAM_ALLY_3 = (1473, 496) # mid
 TEAM_ALLY_4 = (1561, 496) # adc
+# time vermelho: 848; time azul: 2051
+MAP_BLUE_BASE_UL = (1329, 857)
+MAP_BLUE_BASE_LR = (1336, 865)
+# time vermelho: 215; time azul: 133
+MAP_RED_BASE_UL = (1496, 611)
+MAP_RED_BASE_LR = (1507, 619)
+
 
 # champion
 # 9953 - attached, 9378 - skill up, 2345 - casting
