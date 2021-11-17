c9d98ad5c0fb72b6927656766d55737e42036399
wniy <viniberizzi@gmail.com>
2021-11-17 19:52:30 -0300

    remove useless comments

--- src/constants/coordinates.py
+++ src/constants/coordinates.py
@@ -14,17 +14,14 @@ class Coords:
     ALLY_JUNGLE = (1390, 496) 
     ALLY_MID = (1473, 496) 
     ALLY_ADC = (1561, 496) 
-    # time vermelho: 848; time azul: 2051
     MAP_BLUE_BASE_UL_COORD = (1329, 857)
     MAP_BLUE_BASE_LR_COORD = (1336, 865)
-    # time vermelho: 215; time azul: 133
     MAP_RED_BASE_UL_COORD = (1496, 611)
     MAP_RED_BASE_LR_COORD = (1507, 619)
     ALLY_ADC_HEALTH_R_COORD = (1588, 531)
     ALLY_ADC_HEALTH_L_COORD = (1529, 530)
 
     # champion
-    # 9953 - attached, 9378 - skill up, 2345 - casting
     YUUMI_W_UL_COORD = (681, 809) 
     YUUMI_W_LR_COORD = (691, 820)
     YUUMI_LVL_UL = (519, 869)
