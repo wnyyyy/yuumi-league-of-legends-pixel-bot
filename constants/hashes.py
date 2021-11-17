8beeaa901b139cbd08a141fa8bf66ad904c6c4d1
Bruna <bruna.chiochetta@gmail.com>
2021-11-17 19:27:09 -0300

    Add missing files

---
+++ constants/hashes.py
@@ -0,0 +1,34 @@
+SKILL_W_IS_CASTING = 2345
+SKILL_W_IS_UP = 9378
+SKILL_W_IS_ATTACHED = 9953
+RED_SIDE_BLUE_CHECK = 848
+RED_SIDE_RED_CHECK = 215
+BLUE_SIDE_BLUE_CHECK = 2051
+BLUE_SIDE_RED_CHECK = 133
+EMPTY_PIXEL_BAR = 3
+EMPTY_PIXEL_BAR_ALLY = 20
+
+#Yuumi's level hash values 
+LevelBoxHashDict = {
+    3494: 1,
+    5626: 2, 
+    5302: 3, 
+    5303: 4, 
+    6306: 5,
+    7416: 6,
+    4093: 7,
+    7067: 8,
+    6970: 9,
+    7203: 10, 
+    3914: 11,
+    6793: 12,
+    6926: 13, 
+    6088: 14, 
+    7421: 15, 
+    7920: 16,
+    4856: 17, 
+    7523: 18
+}
+
+
+    
