fcf3bc592ae9b05e6484bea69222f2f604cc98ad
wniy <viniberizzi@gmail.com>
2021-11-19 23:32:32 -0300

    add items file

---
+++ src/constants/items.py
@@ -0,0 +1,8 @@
+class Items:
+    GUME_DO_LADRAO_ARCANO = "gume do ladrao arcano"
+    REGENERADOR_DE_PEDRA_LUNAR = "renegerador de pedra lunar"
+    PUTRIFICADOR_QUIMTECH = "putrificador quimtech"
+    TURIBULO_ARDENTE = "turibulo ardente"
+    CAJADO_AQUAFLUXO = "cajado aquafluxo"
+    REDENCAO = "redenção"
+
